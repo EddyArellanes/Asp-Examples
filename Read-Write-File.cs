b5107f41b785037d646f0258fa03778bcddf5014
EddyArellanes <victorywar9999@hotmail.com>
2018-04-16 17:06:44 -0500

    Example to Read and Write in a file
    Using FileStream and FileMode :)

---
+++ Read-Write-File.cs
@@ -0,0 +1,32 @@
+﻿using System; //OS
+using System.Collections.Generic; //Nose Bro diskulpa :v
+using System.IO;
+using System.Linq; //Nose Bro diskulpa :v x2
+using System.Text; //Supongo que para escribir texto en consola
+using System.Threading.Tasks; // Para manejar Hilos 2.0 :D
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new System.IO.FileStream("./MyFile.bin", FileMode.OpenOrCreate);
+            Console.WriteLine("Antes de Procesar");
+            ProcessFile(file);
+            file.Close();
+            Console.WriteLine("Después de Procesar");
+            Console.ReadLine();
+        }
+
+        private static void ProcessFile(FileStream file)
+        {
+            var message = "Hello World";
+            var bytes = Encoding.UTF8.GetBytes(message);
+            for (int i = 0; i < 1000; i++)
+            {
+                file.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
