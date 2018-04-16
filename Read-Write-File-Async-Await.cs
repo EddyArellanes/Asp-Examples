1b39c581e43bdb7a7d742c8a96f9d065bdc8f92d
EddyArellanes <victorywar9999@hotmail.com>
2018-04-16 17:18:04 -0500

    Same functionallity of Write-Read file but using Asynchronous mode

---
+++ Read-Write-File-Async-Await.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text; 
+using System.Threading.Tasks; 
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new System.IO.FileStream("./MyFile.bin", FileMode.OpenOrCreate);
+            Console.WriteLine("Before Process");
+            ProcessFileAsync(file);
+            file.Close();
+            Console.WriteLine("After Process");
+            Console.ReadLine();
+        }
+        //This function open a new Thread when is launched
+        private static async void ProcessFileAsync(FileStream file)
+        {
+            var message = "Hello World";
+            var bytes = Encoding.UTF8.GetBytes(message);
+            for (int i = 0; i < 1000; i++)
+            {
+                
+                await file.WriteAsync(bytes, 0, bytes.Length);
+                Console.WriteLine("Async Process finished");
+            }
+        }
+    }
+}
